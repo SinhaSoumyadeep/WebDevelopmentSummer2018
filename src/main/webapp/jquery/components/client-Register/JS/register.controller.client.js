462f021e5ca7660249e2eff316da50d9ebc8983f
SinhaSoumyadeep <Mayukh@Soumyadeeps-MacBook-Pro.local>
2018-05-14 21:49:00 -0400

    test

--- src/main/webapp/jquery/components/client-Register/JS/register.controller.client.js
+++ src/main/webapp/jquery/components/client-Register/JS/register.controller.client.js
@@ -198,7 +198,7 @@ $(document).ready(function(){
     }
 
 /*
-    this function is used to display success or failure information. //finalized.
+    this function is used to display success or failure information.  //finalized.
 */
     function infoMsgs(msg)
     {
